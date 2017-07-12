ce1f99d2deb2f445b778fcf9efdc1f48459037db
Anton Cataldi <antoncataldi@gmail.com>
2017-07-12 10:52:32 -0400

    remove some create-react-app boilerplate files, add React Component skeleton classes

---
+++ src/GameScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PersonThumbnail from './PersonThumbnail'
+import axios from 'axios'
+
+class GameScreen extends React.Component {
+  constructor(props){
+
+  }
+  componentDidMount() {
+
+  }
+
+  render () {
+    return (        })}
+
+    )
+  }
+}
+
+export default GameScreen
---
+++ src/PersonThumbnail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PersonThumbnail = (props) => {
+
+  return (
+
+  )
+}
+
+export default PersonThumbnail
